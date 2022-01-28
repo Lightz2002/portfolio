fa2f6b9139e22851bf9c4ba6b80de606a7b2abcf
Ryan <kenidyryan@gmail.com>
2022-01-28 10:53:33 +0700

    adding mail using  formspree

--- src/js/script.js
+++ src/js/script.js
@@ -6,50 +6,103 @@ const skillsDescription = document.querySelector(".skills-description");
 const skillsContent = document.querySelector(".skills-content");
 const skillsBar = document.querySelectorAll(".skills-content-bar");
 const clearBtn = document.querySelector(".btn-clear");
+const form = document.querySelector(".contact-form");
 
+const success = "#23C586";
+const successBg = "#d3f3e7";
+const dangerBg = "#f0cdcd";
+const danger = "#B40404";
+
+/* Functions */
 function toggleHamburger(toggle = true) {
-    if (toggle) {
-        hamburgerLine[1].style.transform = "rotate(-45deg) translateX(-6.5px)";
-        hamburgerLine[2].style.transform = "rotate(45deg) translateX(-6.5px)";
-    } else {
-        hamburgerLine[1].style.transform = "";
-        hamburgerLine[2].style.transform = "";
-    }
+  if (toggle) {
+    hamburgerLine[1].style.transform = "rotate(-45deg) translateX(-6.5px)";
+    hamburgerLine[2].style.transform = "rotate(45deg) translateX(-6.5px)";
+  } else {
+    hamburgerLine[1].style.transform = "";
+    hamburgerLine[2].style.transform = "";
+  }
 }
-
-if (hamburgerBtn) {
-    hamburgerBtn.addEventListener("click", function () {
-        hamburgerLine[0].classList.toggle("hidden");
-
-        if (navList.classList.contains("hidden")) {
-            toggleHamburger();
-        } else {
-            toggleHamburger(false);
-        }
-        navList.classList.toggle("hidden");
-    });
-}
-
-window.addEventListener("hashchange", function () {
-    hamburgerLine[0].classList.toggle("hidden");
-    toggleHamburger(false);
-    navList.classList.add("hidden");
-});
 
 const revealSection = function (entries, observer) {
-    const [entry] = entries;
-    if (!entry.isIntersecting) return;
-    skillsDescription.classList.remove("slide-bottom");
-    skillsContent.classList.remove("slide-left");
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  skillsDescription.classList.remove("slide-bottom");
+  skillsContent.classList.remove("slide-left");
 
-    var styleElem = document.head.appendChild(document.createElement("style"));
-    const bar = `.skills-content-bar`;
-    styleElem.innerHTML = `${bar}::after {animation: fill 3s; animation-fill-mode: forwards;}`;
+  var styleElem = document.head.appendChild(document.createElement("style"));
+  const bar = `.skills-content-bar`;
+  styleElem.innerHTML = `${bar}::after {animation: fill 3s; animation-fill-mode: forwards;}`;
 };
 
 const skillsDescriptionObserver = new IntersectionObserver(revealSection, {
-    root: null,
-    threshold: 0,
+  root: null,
+  threshold: 0,
 });
 
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.querySelector(".contact-status");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then((response) => {
+      if (response.ok) {
+        status.innerHTML = "Thanks for your submission!";
+        status.style.backgroundColor = successBg;
+        status.style.color = success;
+        form.reset();
+      } else {
+        response.json().then((data) => {
+          if (Object.hasOwn(data, "errors")) {
+            status.innerHTML = data["errors"]
+              .map((error) => error["message"])
+              .join(", ");
+            status.style.backgroundColor = dangerBg;
+
+            status.style.color = danger;
+          } else {
+            status.innerHTML = "Oops! There was a problem submitting your form";
+            status.style.backgroundColor = dangerBg;
+            status.style.color = danger;
+          }
+        });
+      }
+    })
+    .catch((error) => {
+      status.innerHTML = "Oops! There was a problem submitting your form";
+      status.style.backgroundColor = dangerBg;
+      status.style.color = danger;
+    });
+}
+
+/* Events */
+
+if (hamburgerBtn) {
+  hamburgerBtn.addEventListener("click", function () {
+    hamburgerLine[0].classList.toggle("hidden");
+
+    if (navList.classList.contains("hidden")) {
+      toggleHamburger();
+    } else {
+      toggleHamburger(false);
+    }
+    navList.classList.toggle("hidden");
+  });
+}
+
+window.addEventListener("hashchange", function () {
+  hamburgerLine[0].classList.toggle("hidden");
+  toggleHamburger(false);
+  navList.classList.add("hidden");
+});
+
+form.addEventListener("submit", handleSubmit);
+
+/* Observers */
 skillsDescriptionObserver.observe(skills);
